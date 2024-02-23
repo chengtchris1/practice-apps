049b5522e35f680a2e3c87eccf6fc7df35fd2c21
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-02-22 22:44:37 -0800

    BE Conditionally Complete

--- 2-checkout/server/db.js
+++ 2-checkout/server/db.js
@@ -11,14 +11,85 @@ const connection = mysql.createConnection({
 
 const db = Promise.promisifyAll(connection, { multiArgs: true });
 
+// Three seperate tables or one table?
+// Leaning towards one table.
+// Should I worry about foreign keys/joins? Probably not because basic reqs dont have edit functionality.
+
 db.connectAsync()
   .then(() => console.log(`Connected to MySQL as id: ${db.threadId}`))
   .then(() =>
     // Expand this table definition as needed:
     db.queryAsync(
-      "CREATE TABLE IF NOT EXISTS responses (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY)"
+    `CREATE TABLE IF NOT EXISTS responses (
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      sessionID VARCHAR(256),
+      name VARCHAR(256),
+      email VARCHAR(256),
+      password VARCHAR(256),
+      address1 VARCHAR(256),
+      address2 VARCHAR(256),
+      city VARCHAR(256),
+      shipZip VARCHAR(5),
+      state VARCHAR(2),
+      phone VARCHAR(20),
+      creditCard VARCHAR(16),
+      expiryDate VARCHAR(5),
+      cvv VARCHAR(3),
+      billZip VARCHAR(5)
+      )`
     )
   )
   .catch((err) => console.log(err));
+
+
+db.get = (sessionID)  => {
+  return sessionID === undefined ?
+  db.queryAsync(`select * from responses`) :
+  db.queryAsync(`select * from responses where sessionID = "${sessionID}"`);
+}
+//On "checkout" button press
+db.addNewResponse = (sessionID)=>{
+  return db.queryAsync(`insert into responses (sessionID) values("${sessionID}")`)
+}
+//On form 1 submission
+db.addLogin = (sessionID, name, email, password)=>{
+  /*
+  INCORRECT: Notice how I swapped the ordering:
+  `update responses where sessionID = ${sessionID} set name = ${name}, email =${email} 'password' = ${password}`
+  */
+  return db.queryAsync(
+    `update responses set name = "${name}",
+    email = "${email}", password = "${password}"
+
+    where sessionID = "${sessionID}"`);
+}
+//On form 2 submission
+db.addAddress = (sessionID, address1, address2, city, state, shipZip, phone)=>{
+  return db.queryAsync(`
+  update responses
+  set
+  address1 = "${address1}",
+  address2 = "${address2}",
+  city = "${city}",
+  state = "${state}",
+  shipZip = "${shipZip}",
+  phone="${phone}"
+  where
+  sessionID = "${sessionID}"`);
+}
+//On form 3 submission
+db.addCreditCard = (sessionID, creditCard, expiryDate, cvv, billZip) =>{
+  return db.queryAsync(`
+  update responses
+
+  set
+  creditCard = "${creditCard}",
+  expiryDate = "${expiryDate}",
+  cvv = "${cvv}",
+  billZip = "${billZip}"
+
+  where
+  sessionID = "${sessionID}"`);
+}
 
 module.exports = db;
--- 2-checkout/server/index.js
+++ 2-checkout/server/index.js
@@ -3,22 +3,102 @@ const express = require("express");
 const path = require("path");
 const sessionHandler = require("./middleware/session-handler");
 const logger = require("./middleware/logger");
-
-// Establishes connection to the database on server start
-const db = require("./db");
-
+const db = require("./db.js");
+// Establishes connection to the database on server start/const db = require("./db");
 const app = express();
+app.use(express.json())
 
 // Adds `req.session_id` based on the incoming cookie value.
 // Generates a new session if one does not exist.
-app.use(sessionHandler);
 
+app.use(sessionHandler);
 // Logs the time, session_id, method, and url of incoming requests.
 app.use(logger);
-
 // Serves up all static and generated assets in a specified folder.
-app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
+console.log(path.join(__dirname, '../client/dist'))
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
+//If entry does not exist
+app.post('/checkout', (req, res)=>{
+  console.log(req.body.session)
+  db.addNewResponse(req.body.session)
+  .then((data)=>{
+    console.log(data)
+    res.send(`Added session ${req.body.session} to database`);
+    res.status(200);
+  })
+  .catch((data)=>{
+    console.log(data)
+    res.status(500)
+  })
+  .finally(()=>{
+    res.end();
+  })
+})
+
+//To update with new info was user scrolls through form.
+app.get('/checkout', (req, res)=>{
+  let id = req.query.sessionID
+  db.get(req.query.sessionID)
+  .then((data)=>{
+    id === undefined ? res.send(data[0]) : res.send(data[0][0])
+  })
+  .catch((err)=>{
+    res.status(400);
+    res.send(err);
+    console.log(err);})
+  .finally(()=>{
+    res.end()
+  })
+})
+
+app.patch('/checkout/:sessionID/:formID', (req, res)=>{
+  let id = req.params.sessionID;
+  console.log(typeof req.params.formID);
+  if(req.params.formID === '1'){
+    db.addLogin(id, req.body.name, req.body.email, req.body.password)
+      .then((data)=>{
+        res.send("Changed login info.")
+        res.end();
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+
+  } else if (req.params.formID === '2'){
+    //sessionID, address1, address2, city, state, shipzip, phone
+    db.addAddress(id, req.body.address1, req.body.address2, req.body.city, req.body.state, req.body.shipZip, req.body.phone)
+    .then((data)=>{
+      res.send("Changed address info.");
+      res.end();
+      res.end();
+      console.log(data)}
+      )
+    .catch((err)=>{console.log(err)})
+
+  } else if (req.params.formID === '3'){
+    //(sessionID, creditCard, expirydate, cvv, billZip)
+    db.addCreditCard(id, req.body.creditCard, req.body.expiryDate, req.body.cvv, req.body.billZip)
+    .then((data)=>{
+      res.send("Changed payment info")
+      console.log(data)
+      res.end();
+    })
+    .catch((err)=>{console.log(err)})
+  } else {
+    res.send("Error")
+    res.status(400);
+    res.end()
+  }
+
+  })
+
+/*
+app.get('/checkout', (req, res)=>{
+res.send("Hello world")
+res.end();
+
+})*/
 /****
  *
  *
